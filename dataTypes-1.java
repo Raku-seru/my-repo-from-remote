674b2e29be4df26fde63f82c4653469ce56d50fa
Raxel AK <79732801+Raku-seru@users.noreply.github.com>
2021-03-09 19:21:10 +0700

    Add dataTypes-1 java (Uncompiled)
    Optional files. Contain a list of Data Types used in java and can be accessed for notes about detailed Data Type specification.

---
+++ dataTypes-1.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.BufferedReader;import java.io.InputStreamReader;import java.io.IOException;
+
+public class dataTypes {
+	public static void main(String[] args) {
+		byte valueByte = 10;
+		/*	Size 8-bit
+			Range : -128 to 127, default 0
+			Ratio compare to Int (4 bytes : 1 int) */
+		
+		short valueShort = 10;
+		/*	Size 16-bit
+			Range : -32768 to 32767, default 0
+			Ratio compare to Int (2 shorts : 1 int) */
+			
+		int valueInt = 100;
+		/*	Size 32-bit
+			Range : -2,147,483,648 to 2,147,483,647, default 0
+			Ratio is 1:1 to int */
+			
+		long valueLong = 10000L;
+		/*	Size 64-bit
+			Range :  -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807, default 0L
+			Example : 1969L, -53420L */
+			
+		float valueFloat = 6.9f;
+		/*	Can contain decimals,default 0.0f Ex : 3.5f,99.9f */
+		
+		double valueDouble - 5.0d;
+		/* Same as float but has bigger capacity, default 0.0d */
+		
+		boolean valueTrue = True;
+		boolean valueFalse = False;
+		/* Only contain 'True' or 'False',default 'False' */
+		
+		char valueChar = 'A';
+		/* Only contain a character and contained between '' */
+		
+		}
+	}
